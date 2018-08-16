2d8a0bc156fadfdfa98f0ea60eb728f0768cd184
Richard Boardman <rickschmoo@gmail.com>
2018-08-15 20:15:06 -0700

    Made mock logo a Link tag to redirect depending on login status

---
+++ client/src/components/Landing.js
@@ -0,0 +1,25 @@
+// import React, { Component} from 'react';
+// import { connect } from 'react-redux';
+import React from 'react';
+
+// class Landing extends Component {
+
+const Landing = () => {
+	return (
+		<div style={{ textAlign: 'center' }}>
+		  	<h1>
+		  		Emaily for Emily!
+		  	</h1>
+		  	Collect feedback from your users!
+		</div>
+	);
+}
+
+export default Landing;
+
+// // redux wiring
+// function mapStateToProps( { auth } ) {
+// 	return { auth };
+// }
+
+// export default connect(mapStateToProps)(Header);
