d8e40282c9be2f0c4ea3151495245e4b51acd21b
Richard Boardman <rickschmoo@gmail.com>
2018-08-15 20:01:51 -0700

    Wired up login/logout buttons in the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,46 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+
+	renderContent() {
+		switch (this.props.auth) {
+
+			case null:
+				// return 'Still deciding';
+				return;
+
+			case false:
+				//return "I'm logged out";
+				return <li><a href="/auth/google">Login with Google</a></li>;
+			default:
+				// return ("I'm logged in!: <"
+				// 	+ this.props.auth.googleID + 
+				// ">");
+				return <li><a href="/api/logout">Logout</a></li>;
+		}
+	}
+
 	render() {
+		console.log("HEADER RENDER PROPS: ", this.props);
 		return (
 			  <nav>
 			    <div className="nav-wrapper">
 			      <a href="#" className="left brand-logo">Emaily</a>
-			      <ul id="nav-mobile" className="right hide-on-med-and-down">
-			        <li><a href="/">Login with Google</a></li>
-			      </ul>
+
+			      	<ul id="nav-mobile" className="right hide-on-med-and-down">
+			      		{this.renderContent()}
+			      	</ul>
 			    </div>
 			  </nav>
 		);
 	}
 }
 
-export default Header;
+// redux wiring
+function mapStateToProps( { auth } ) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,15 @@
 // records whether or not the user is logged in
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
 
-	console.log('AUTH REDUCER: ', action);
-	
+// return null if we don't know auth state
+export default function(state = null, action) {
+
+	console.log('ACTION @ AUTH REDUCER: ', action);
+
 	switch (action.type) {
+
+		case FETCH_USER:
+			return action.payload || false;
 
 		default:
 			return state;
