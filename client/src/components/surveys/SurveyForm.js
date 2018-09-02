08355388bb5d1c3f8e70172bf41e004ecad823b8
Richard Boardman <rickschmoo@gmail.com>
2018-09-02 14:21:51 -0700

    Basic form submit scaffolding

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+
+class SurveyForm extends Component {
+
+	render() {
+		return(
+			<div>
+				SurveyForm!
+				<form onSubmit={this.props.handleSubmit(values =>
+					console.log("NewSurveyFormSubmit: ", values))}>
+					<Field 
+						type="text"
+						name="surveyTitle"
+						component="input"
+					/>
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default reduxForm({
+	form: 'surveyForm'
+})(SurveyForm);
+
---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+class SurveyFormReview extends Component {
+
+	render() {
+		return(
+			<div>
+				SurveyFormReview
+			</div>
+		);
+	}
+}
+
+export default SurveyFormReview;
